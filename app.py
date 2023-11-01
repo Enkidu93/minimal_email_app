0b1a2defc55636527026980d9a9b6b276d8b9b41
Enkidu93 <lowryec17@gcc.edu>
2023-11-01 13:32:29 -0400

    switch to ssl

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import smtplib
 from email.mime.text import MIMEText
-import os
+import os, ssl
 
 
 email_receiver = st.text_input('To')
@@ -15,13 +15,12 @@ if st.button("Send Email"):
         msg['To'] = email_receiver
         msg['Subject'] = subject
 
-        server = smtplib.SMTP('mail.languagetechnology.org', 587)
+        context = ssl.create_default_context()
+        server = smtplib.SMTP_SSL('mail.languagetechnology.org', 465, context=context)
         server.set_debuglevel(1)
-        server.starttls()
         server.login("serval-app@languagetechnology.org",os.environ.get('SERVAL_APP_EMAIL_PASSWORD') )
         server.sendmail("serval-app@languagetechnology.org", email_receiver, msg.as_string())
         server.quit()
-
         st.success('Email sent successfully! 🚀')
     except Exception as e:
         st.error(f"Failed to send email: {e}")
